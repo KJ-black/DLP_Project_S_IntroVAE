ee51a0168f5decac05bd6ca8fede3a92ff998850
James24029775 <heartstonejames@gmail.com>
2021-09-05 10:23:55 +0800

    Update S-IntroVAE.py

--- models/S-IntroVAE.py
+++ models/S-IntroVAE.py
@@ -1,6 +1,9 @@
 """
 Models
 """
+import torch
+import torch.nn as nn
+
 class _Residual_Block(nn.Module):
     """
     https://github.com/hhb072/IntroVAE
@@ -168,3 +171,16 @@ class SoftIntroVAE(nn.Module):
         else:
             y = self.decoder(z)
         return y
+
+def reparameterize(mu, logvar):
+    """
+    This function applies the reparameterization trick:
+    z = mu(X) + sigma(X)^0.5 * epsilon, where epsilon ~ N(0,I)
+    :param mu: mean of x
+    :param logvar: log variaance of x
+    :return z: the sampled latent variable
+    """
+    device = mu.device
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std).to(device)
+    return mu + eps * std
