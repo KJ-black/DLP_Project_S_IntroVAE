b279dabfb2e021589dad5f55be5e32991f3472e0
KJ-black <alanchen0922@gmail.com>
2021-09-06 10:56:47 +0800

    Update Conditional Code

---
+++ Conditional Soft-IntroVAE/utils.py
@@ -0,0 +1,101 @@
+# imports for the tutorial
+import os
+import time
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+# pytorch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from torchvision.utils import make_grid
+from torchvision import transforms
+import torchvision.utils as vutils
+
+"""
+Helper Functions
+"""
+
+def calc_kl(logvar, mu, mu_o=0.0, logvar_o=0.0, reduce='sum'):
+    """
+    Calculate kl-divergence
+    :param logvar: log-variance from the encoder
+    :param mu: mean from the encoder
+    :param mu_o: negative mean for outliers (hyper-parameter)
+    :param logvar_o: negative log-variance for outliers (hyper-parameter)
+    :param reduce: type of reduce: 'sum', 'none'
+    :return: kld
+    """
+    if not isinstance(mu_o, torch.Tensor):
+        mu_o = torch.tensor(mu_o).to(mu.device)
+    if not isinstance(logvar_o, torch.Tensor):
+        logvar_o = torch.tensor(logvar_o).to(mu.device)
+    kl = -0.5 * (1 + logvar - logvar_o - logvar.exp() / torch.exp(logvar_o) - (mu - mu_o).pow(2) / torch.exp(
+        logvar_o)).sum(1)
+    if reduce == 'sum':
+        kl = torch.sum(kl)
+    elif reduce == 'mean':
+        kl = torch.mean(kl)
+    return kl
+
+
+def reparameterize(mu, logvar):
+    """
+    This function applies the reparameterization trick:
+    z = mu(X) + sigma(X)^0.5 * epsilon, where epsilon ~ N(0,I)
+    :param mu: mean of x
+    :param logvar: log variaance of x
+    :return z: the sampled latent variable
+    """
+    device = mu.device
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std).to(device)
+    return mu + eps * std
+
+
+def calc_reconstruction_loss(x, recon_x, loss_type='mse', reduction='sum'):
+    """
+
+    :param x: original inputs
+    :param recon_x:  reconstruction of the VAE's input
+    :param loss_type: "mse", "l1", "bce"
+    :param reduction: "sum", "mean", "none"
+    :return: recon_loss
+    """
+    if reduction not in ['sum', 'mean', 'none']:
+        raise NotImplementedError
+    recon_x = recon_x.view(recon_x.size(0), -1)
+    x = x.view(x.size(0), -1)
+    if loss_type == 'mse':
+        recon_error = F.mse_loss(recon_x, x, reduction='none')
+        recon_error = recon_error.sum(1)
+        if reduction == 'sum':
+            recon_error = recon_error.sum()
+        elif reduction == 'mean':
+            recon_error = recon_error.mean()
+    elif loss_type == 'l1':
+        recon_error = F.l1_loss(recon_x, x, reduction=reduction)
+    elif loss_type == 'bce':
+        recon_error = F.binary_cross_entropy(recon_x, x, reduction=reduction)
+    else:
+        raise NotImplementedError
+    return recon_error
+
+
+def load_model(model, pretrained, device):
+    weights = torch.load(pretrained, map_location=device)
+    model.load_state_dict(weights['model'], strict=False)
+
+
+def save_checkpoint(model, epoch, iteration, prefix=""):
+    model_out_path = "./saves/" + prefix + "model_epoch_{}_iter_{}.pth".format(epoch, iteration)
+    state = {"epoch": epoch, "model": model.state_dict()}
+    if not os.path.exists("./saves/"):
+        os.makedirs("./saves/")
+
+    torch.save(state, model_out_path)
+
+    print("model checkpoint saved @ {}".format(model_out_path))
