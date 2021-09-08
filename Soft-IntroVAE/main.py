c863d9a369715151c701a707806a80e3f2570508
James24029775 <74500141+James24029775@users.noreply.github.com>
2021-09-08 11:20:16 +0800

    Add files via upload

---
+++ Soft-IntroVAE/main.py
@@ -0,0 +1,21 @@
+import torch
+from train_soft_intro_vae import train_soft_intro_vae
+
+if __name__ == "__main__":
+    # hyperparameters
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("device:", device)
+    num_epochs = 100
+    lr = 2e-4
+    batch_size = 64
+    beta_kl = 1.0
+    beta_rec = 1.0
+    beta_neg = 256
+    img_size = 64
+    with_fid = False
+    test_iter = 1000
+    model = train_soft_intro_vae(dataset='102flowers', z_dim=128, lr_e=2e-4, lr_d=2e-4, batch_size=batch_size,
+                                num_workers=0, start_epoch=0, num_epochs=num_epochs, num_vae=0, save_interval=5000,
+                                recon_loss_type="mse", beta_kl=beta_kl, beta_rec=beta_rec, beta_neg=beta_neg,
+                                img_size=img_size, with_fid=with_fid, test_iter=test_iter, seed=-1, pretrained=None, 
+                                device=device)
